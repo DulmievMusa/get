c1be9d79f7d742f1be2aebd5529a13c782b74401
Musa <musadulmiev@gmail.com>
2026-02-11 18:48:51 +0300

    Автоматическая подсветка

---
+++ get-led/auto-light.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+
+
+GPIO.setup(led, GPIO.OUT)
+
+photo = 6
+
+GPIO.setup(photo, GPIO.IN)
+
+
+while True:
+    state = not GPIO.input(photo)
+    GPIO.output(led, state)
